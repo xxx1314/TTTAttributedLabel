aa8fb2ecca7e25c4430ea168b5c39c26915ab135
why <why@163.com>
2019-07-22 13:57:57 +0800

    创建person类

---
+++ Example/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  Espressos
+//
+//  Created by hello on 2019/7/22.
+//  Copyright © 2019年 Gowalla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ Example/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  Espressos
+//
+//  Created by hello on 2019/7/22.
+//  Copyright © 2019年 Gowalla. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
